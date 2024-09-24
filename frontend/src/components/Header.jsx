5d7cbfc48f4f3d07aa51de884604e24614931018
BoonMoon <randomskitztm@gmail.com>
2024-09-23 23:51:50 -0400

    Created Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,16 @@
 import HeaderButton from "./HeaderButton";
 
 export default function Header() {
-    return <header className="bg-gray-900 flex justify-center place-items-center p-4 gap-10">
-        <HeaderButton name="Home" link="/"/>
-        <HeaderButton name="Games" link="/"/>
-        <img className="max-w-48 cursor-pointer" src="./MainIcon.svg"/>
-        <HeaderButton name="Support" link="/"/>
-        <HeaderButton name="Shop" link="/"/>
-    </header>
+    return (
+        <header className="relative flex justify-center p-4 gap-10 bg-gray-900 h-60">
+            <div className="absolute inset-0 bg-[url('/HeaderBG.png')] bg-cover bg-center blur-[8px]"></div>
+            <div className="relative z-10 flex gap-10 items-center">
+                <HeaderButton name="Home" link="/" />
+                <HeaderButton name="Games" link="/" />
+                <img className="max-w-48 cursor-pointer" src="./MainIcon.svg" />
+                <HeaderButton name="Support" link="/" />
+                <HeaderButton name="Shop" link="/" />
+            </div>
+        </header>
+    );
 }
