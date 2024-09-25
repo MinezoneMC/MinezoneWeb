b07f42c9a55ec6fab7f5d75ce0ddb19fec3f9a78
Suryansh <suryanshsaroch5@gmail.com>
2024-09-24 23:08:34 -0400

    auth set up

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path,include
-from . import views
-from django.contrib.auth import views as auth_views
-from .views import LoginView, ReactView, SignupView
-
+from .views import LoginView, ReactView, SignupView, ForgotPasswordView, ResetPasswordView
 
 
 urlpatterns = [
     path('', ReactView.as_view(), name='home'),  
-    path('signup', SignupView.as_view() , name='signup'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('support/', views.support, name='support'), 
-    path('forum/', views.forum, name='forum'),
-    path('game/', views.game, name='game'),  
-    path('login', LoginView.as_view(), name="login"),
+    path("signup", SignupView.as_view(), name="signup"),
+    path("login", LoginView.as_view(), name="login"),
+    path("forgotPassword", ForgotPasswordView.as_view(), name="forgotPassword"),
+    path("resetPassword", ResetPasswordView.as_view(), name="resetPassword"),
 ] 
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -12,8 +12,33 @@ from django.contrib.auth.hashers import make_password
 from rest_framework import status
 from django.core.mail import send_mail
 import hashlib
+from django.conf import settings
+from datetime import datetime, timedelta
+import hashlib
+import uuid
+from django.utils import timezone
 
-SALT = "8b4f6b2cc1868d75ef79e5cfb8779c11b6a374bf0fce05b485581bf4e1e25b96c8c2855015de8449"
+
+URL = "http://localhost:8000"
+
+def mail_template(content, button_url, button_text):
+    return f"""<!DOCTYPE html>
+            <html>
+            <body style="text-align: center; font-family: "Verdana", serif; color: #000;">
+                <div style="max-width: 600px; margin: 10px; background-color: #fafafa; padding: 25px; border-radius: 20px;">
+                <p style="text-align: left;">{content}</p>
+                <a href="{button_url}" target="_blank">
+                    <button style="background-color: #444394; border: 0; width: 200px; height: 30px; border-radius: 6px; color: #fff;">{button_text}</button>
+                </a>
+                <p style="text-align: left;">
+                    If you are unable to click the above button, copy paste the below URL into your address bar
+                </p>
+                <a href="{button_url}" target="_blank">
+                    <p style="margin: 0px; text-align: left; font-size: 10px; text-decoration: none;">{button_url}</p>
+                </a>
+                </div>
+            </body>
+            </html>"""
 
 class ReactView(APIView):
     serializer_class = ReactSerializer
@@ -24,7 +49,6 @@ class ReactView(APIView):
         
         # Serialize posts
         serializer = self.serializer_class(posts, many=True)
-        
         # Check if the user can post
         can_post = request.user.groups.filter(name='CanMakePosts').exists()
 
@@ -55,15 +79,17 @@ class LoginView(APIView):
     def post(self, request, format=None):
         email = request.data["email"]
         password = request.data["password"]
-        hashed_password = make_password(password=password, salt=SALT)
+        salt = uuid.uuid4().hex # Generate a unique salt for each user
+        hashed_password = make_password(password=password, salt=salt)
         user = User.objects.get(email=email)
+
         if user is None or user.password != hashed_password:
             return Response(
                 {
                     "success": False,
                     "message": "Invalid Login Credentials!",
                 },
-                status=status.HTTP_200_OK,
+                status=status.HTTP_400_BAD_REQUEST,
             )
         else:
             return Response(
@@ -74,14 +100,104 @@ class LoginView(APIView):
 
 class SignupView(APIView):
     def post(self, request, format=None):
+        salt = uuid.uuid4().hex
         request.data["password"] = make_password(
-            password=request.data["password"], salt=SALT
+            password=request.data["password"], salt=salt
         )
+
         serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
+        
+        if serializer.is_valid():   
             serializer.save()
             return Response(
                 {"success": True, "message": "You are now registered on our website!"},
+                status=status.HTTP_200_OK,
+            )
+        else:
+            return Response(
+                {"success": False, "message": serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+class ResetPasswordView(APIView):
+    def post(self, request, format=None):
+        user_id = request.data["id"]
+        token = request.data["token"]
+        password = request.data["password"]
+
+        token_obj = Token.objects.filter(
+            user_id=user_id).order_by("-created_at")[0]
+        if token_obj.expires_at < timezone.now():
+            return Response(
+                {
+                    "success": False,
+                    "message": "Password Reset Link has expired!",
+                },
+                status=status.HTTP_200_OK,
+            )
+        elif token_obj is None or token != token_obj.token or token_obj.is_used:
+            return Response(
+                {
+                    "success": False,
+                    "message": "Reset Password link is invalid!",
+                },
+                status=status.HTTP_200_OK,
+            )
+        else:
+            token_obj.is_used = True
+            hashed_password = make_password(password=password, salt=SALT)
+            ret_code = User.objects.filter(
+                id=user_id).update(password=hashed_password)
+            if ret_code:
+                token_obj.save()
+                return Response(
+                    {
+                        "success": True,
+                        "message": "Your password reset was successfully!",
+                    },
+                    status=status.HTTP_200_OK,
+                )
+
+
+class ForgotPasswordView(APIView):
+    def post(self, request, format=None):
+        email = request.data["email"]
+        user = User.objects.get(email=email)
+        created_at = timezone.now()
+        expires_at = timezone.now() + timezone.timedelta(1) # token expires in 1 day
+        salt = uuid.uuid4().hex # generates a random UUID, which is a 128-bit value. Which is then converted to a string of hex digits.
+        token = hashlib.sha512(
+            (str(user.id) + user.password + created_at.isoformat() + salt).encode(
+                "utf-8"
+            )
+        ).hexdigest()
+        token_obj = {
+            "token": token,
+            "created_at": created_at,
+            "expires_at": expires_at,
+            "user_id": user.id,
+        }
+        serializer = TokenSerializer(data=token_obj)
+        if serializer.is_valid():
+            serializer.save()
+            subject = "Forgot Password Link"
+            content = mail_template(
+                "We have received a request to reset your password. Please reset your password using the link below.",
+                f"{URL}/resetPassword?id={user.id}&token={token}",
+                "Reset Password",
+            )
+            send_mail(
+                subject=subject,
+                message=content,
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[email],
+                html_message=content,
+            )
+            return Response(
+                {
+                    "success": True,
+                    "message": "A password reset link has been sent to your email.",
+                },
                 status=status.HTTP_200_OK,
             )
         else:
@@ -89,18 +205,9 @@ class SignupView(APIView):
             for key in serializer.errors:
                 error_msg += serializer.errors[key][0]
             return Response(
-                {"success": False, "message": error_msg},
+                {
+                    "success": False,
+                    "message": error_msg,
+                },
                 status=status.HTTP_200_OK,
             )
-
-
-
-def support(request):
-    return render(request, "main/support.html")
-
-def forum(request):
-    return render(request, "main/forum.html")
-
-def game(request):
-    return render(request, "main/game.html")
-
