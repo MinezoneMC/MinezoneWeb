e752899b9c641e01af4c5296ed04c62924961391
surysaroch <suryanshsaroch5@gmail.com>
2024-11-01 20:35:14 -0400

    cors issue hopefully fixed now

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -2,17 +2,15 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('api/', include([
-        path('', ReactView.as_view(), name='home'),
-        path("signup/", SignupView.as_view(), name="signup"),
-        path("login/", LoginView.as_view(), name="login"),
-        path("forgotPassword/", ForgotPasswordView.as_view(), name="forgotPassword"),
-        path("resetPassword/", ResetPasswordView.as_view(), name="resetPassword"),
-        path("forum/", ForumView.as_view(), name="forum"),
-        path('profile/<int:user_id>/', UserProfileView.as_view(), name='profile'),
-        path("forum/<int:forum_id>/", ForumDetailView.as_view(), name="forum_detail"),
-        path('forums/<int:forum_id>/comments/', CommentView.as_view(), name="comment"),
-        path('users/<int:user_id>/', UserPublicProfileView.as_view(), name='user_public_profile'),
-        path('supports/', UserSupport.as_view(), name='user_support'),
-    ])),
+    path('', ReactView.as_view(), name='home'),
+    path("api/signup/", SignupView.as_view(), name="signup"),
+    path("api/login/", LoginView.as_view(), name="login"),
+    path("api/forgotPassword/", ForgotPasswordView.as_view(), name="forgotPassword"),
+    path("api/resetPassword/", ResetPasswordView.as_view(), name="resetPassword"),
+    path("api/forum/", ForumView.as_view(), name="forum"),
+    path('api/profile/<int:user_id>/', UserProfileView.as_view(), name='profile'),
+    path("api/forum/<int:forum_id>/", ForumDetailView.as_view(), name="forum_detail"),
+    path('api/forums/<int:forum_id>/comments/', CommentView.as_view(), name="comment"),
+    path('api/users/<int:user_id>/', UserPublicProfileView.as_view(), name='user_public_profile'),
+    path('api/supports/', UserSupport.as_view(), name='user_support'),
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -50,9 +50,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware', #This is for cross-origin requests
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
